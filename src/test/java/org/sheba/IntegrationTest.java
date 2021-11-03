3f041ebebd12f0e77ef0428a647a78540fabd792
Alamgir Kabir <a.kabir@sheba.xyz>
2021-11-03 10:35:00 +0600

    Initial version of performance generated by generator-jhipster@7.2.0

---
+++ src/test/java/org/sheba/IntegrationTest.java
@@ -0,0 +1,20 @@
+package org.sheba;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.sheba.PerformanceApp;
+import org.sheba.ReactiveSqlTestContainerExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = PerformanceApp.class)
+@ExtendWith(ReactiveSqlTestContainerExtension.class)
+public @interface IntegrationTest {
+}
